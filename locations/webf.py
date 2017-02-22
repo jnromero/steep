bbc878b998a11db2f9831f3b6558b0fe15549df7
jnromero <jnromero@gmail.com>
2017-02-22 11:14:20 -0700

    Deleted Locations

--- locations/webf.py
+++
@@ -1,9 +0,0 @@
-def getLocation(config):
-	config['webServerRoot']="/home/jnromero/Sites/jnromero.com/Experiments/"
-	config['serverType']="regularExperiment"	
-	config['serverPort']=24921
-	config['webSocketPort']=13557
-	config["domain"]="http://jnromero.com/experiment"
-	config["websocketURL"]='ws://jnromero.com/webSockets/experiment'
-	config["screenServer"]="http://jnromero.com/screenServer/"
-	return config
