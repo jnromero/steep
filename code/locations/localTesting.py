f034d4c200d1a253bbb9e56daee4991acb2117d3
jnromero <jnromero@gmail.com>
2018-03-13 19:40:17 -0700

    Deleting unnecessary files

--- code/locations/localTesting.py
+++
@@ -1,8 +0,0 @@
-def getLocation(config,ip="localhost"):
-	config['webServerRoot']="/Users/jnr/Dropbox/Sites/jnromero.com/Experiments/"
-	config['serverType']="regularExperiment"	
-	config['serverPort']=24921
-	config['webSocketPort']=13557
-	config["domain"]="http://jnromero.com/testing"
-	config["websocketURL"]="ws://jnromero.com/testingWS"
-	return config
