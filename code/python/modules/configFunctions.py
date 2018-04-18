1e0c3132899f3e69dc2268fb71208aedf1437507
jnromero <jnromero@gmail.com>
2018-04-17 21:19:39 -0700

    Update messagesLog.pickle location
    Moved messagesLog.pickle to a folder called messages/ in the logs/
    folder, and added a python file name messageLogViewer.py

--- code/python/modules/configFunctions.py
+++ code/python/modules/configFunctions.py
@@ -15,14 +15,39 @@ def setOtherFileLocations(config):
 
 	config['fullDataFolder']=config['webServerRoot']+config['dataFolder']
 	config['logFolder']=config['fullDataFolder']+"/logs/"
-	config['messageLogFile']=config['logFolder']+"/messagesLog.pickle"
+	config['messageLogFile']=config['logFolder']+"/messages/messagesLog.pickle"
 	config['twistedLogFile']=config['logFolder']+"/twistedLog.log"
 	config['fullLogFile']=config['logFolder']+"/fullLog.pickle"
 
 	makeFolderIfNeeded(config['fullDataFolder'])
 	makeFolderIfNeeded(config['logFolder'])
+	makeFolderIfNeeded(config['logFolder']+"messages/")
 	makeFolderIfNeeded(config['logFolder']+"txt/")
 	makeFolderIfNeeded(config['logFolder']+"pickle/")
+
+
+	string="""import pickle 
+filename='messagesLog.pickle'
+with open(filename,'rb') as f:
+	while True:
+		try:
+			data=pickle.load(f)
+			print data
+			print data[0],data[1],data[2]['type'],data[2]['sid']
+			raw_input() 
+		except Exception,e: 
+			print str(e)
+			break
+"""
+
+	filename=config['logFolder']+"/messages/messagesLogViewer.py"
+	file = open(filename,'w')
+	file.writelines(string)
+	file.close() 
+
+
+
+
 
 	return config
 
