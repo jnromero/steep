ece5c3e5dfc0178dfe7a46d2927a4d97aa47e7d8
Julian Romero <jnromero@gmail.com>
2020-05-27 12:13:49 -0700

    update viewer

--- code/python/modules/viewer.py
+++ code/python/modules/viewer.py
@@ -9,7 +9,40 @@ def getData():
 	file.close() 
 	return data
 
+def showInfo(data):
+	info=[]
+	for k in data:
+		thisInfo=[k,type(data[k]).__name__]
+		info.append(thisInfo)
+	info.sort()
+	info.sort(key=lambda x: x[1])
+
+	thisNumber=max([len(x[0]) for x in info])
+	print("-"*(thisNumber+20))
+	print(" "*(thisNumber-7)+"self.data") 
+	print("-"*(thisNumber+20))
+	for k in info:
+		print(" "*(thisNumber-len(k[0])+2)+str(k[0])+" | "+str(k[1]))
+	print() 
+	if len(data['subjectIDs'])>0:
+		info=[]
+		sid1=data['subjectIDs'][0]
+		subjectClass=data['subjects'][sid1]
+		for k in dir(subjectClass):
+			thisInfo=[k,type(eval("subjectClass.%s"%(k))).__name__]
+			info.append(thisInfo)
+		info.sort()
+		info.sort(key=lambda x: x[1])
+		thisNumber=max([len(x[0]) for x in info])
+		print("-"*(thisNumber+20))
+		print(" "*(thisNumber-10)+"subjectClass") 
+		print("-"*(thisNumber+20))
+		for k in info:
+			if k[0][0]!="_" and k[1]!="method":
+				print(" "*(thisNumber-len(k[0])+2)+str(k[0])+" | "+str(k[1]))
+	print() 
+
+	
 if __name__ == '__main__':
 	data=getData()
-	for sid in data['subjectIDs']:
-		print(sid) 
+	showInfo(data)
